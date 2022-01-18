15642dd8b90335456ad3ad3302f52db8a88dddf5
chenjl <chenjl@augurit.com>
2022-01-18 09:06:59 +0800

    更新注释

--- inference.py
+++ inference.py
@@ -13,38 +13,14 @@ for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
     
 if __name__ == "__main__":
-    #-------------------------------------------------------------------------#
-    #   如果想要修改对应种类的颜色，到generate函数里修改self.colors即可
-    #-------------------------------------------------------------------------#
     deeplab = DeeplabV3()
-    #----------------------------------------------------------------------------------------------------------#
-    #   mode用于指定测试的模式：
-    #   'predict'表示单张图片预测，如果想对预测过程进行修改，如保存图片，截取对象等，可以先看下方详细的注释
-    #   'video'表示视频检测，可调用摄像头或者视频进行检测，详情查看下方注释。
-    #   'fps'表示测试fps，使用的图片是img里面的street.jpg，详情查看下方注释。
-    #   'dir_predict'表示遍历文件夹进行检测并保存。默认遍历img文件夹，保存img_out文件夹，详情查看下方注释。
-    #----------------------------------------------------------------------------------------------------------#
     mode = "predict"
-    #-------------------------------------------------------------------------#
-    #   video_path用于指定视频的路径，当video_path=0时表示检测摄像头
-    #   video_save_path表示视频保存的路径，当video_save_path=""时表示不保存
-    #   video_fps用于保存的视频的fps
-    #   video_path、video_save_path和video_fps仅在mode='video'时有效
-    #   保存视频时需要ctrl+c退出才会完成完整的保存步骤，不可直接结束程序。
-    #-------------------------------------------------------------------------#
+    # 视频语义分割
     video_path      = 0
     video_save_path = ""
     video_fps       = 25.0
-    #-------------------------------------------------------------------------#
-    #   test_interval用于指定测量fps的时候，图片检测的次数
-    #   理论上test_interval越大，fps越准确。
-    #-------------------------------------------------------------------------#
+    #test_interval用于指定测量fps的时候，图片检测的次数 理论上test_interval越大，fps越准确。
     test_interval = 100
-    #-------------------------------------------------------------------------#
-    #   dir_origin_path指定了用于检测的图片的文件夹路径
-    #   dir_save_path指定了检测完图片的保存路径
-    #   dir_origin_path和dir_save_path仅在mode='dir_predict'时有效
-    #-------------------------------------------------------------------------#
     dir_origin_path = "img/"
     dir_save_path   = "img_out/"
 
@@ -63,15 +39,10 @@ if __name__ == "__main__":
         fps = 0.0
         while(True):
             t1 = time.time()
-            # 读取某一帧
             ref,frame=capture.read()
-            # 格式转变，BGRtoRGB
             frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-            # 转变成Image
             frame = Image.fromarray(np.uint8(frame))
-            # 进行检测
             frame = np.array(deeplab.detect_image(frame))
-            # RGBtoBGR满足opencv显示格式
             frame = cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
             
             fps  = ( fps + (1./(time.time()-t1)) ) / 2
