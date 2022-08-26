fbdfa9f436f33629b13b80de3b939e5b0dcf64e6
chenjl <chenjl@augurit.com>
2022-08-26 10:52:07 +0800

    添加推理过程的文档

--- pytorch/backbone/sync_batchnorm/__init__.py
+++ pytorch/backbone/sync_batchnorm/__init__.py
@@ -1,16 +1,2 @@
 from .batchnorm import SynchronizedBatchNorm1d, SynchronizedBatchNorm2d, SynchronizedBatchNorm3d
 from .replicate import DataParallelWithCallback, patch_replication_callback
-from backbone import resnet, xception, drn, mobilenet
-
-
-def build_backbone(backbone, output_stride, BatchNorm):
-    if backbone == 'resnet':
-        return resnet.ResNet101(output_stride, BatchNorm)
-    elif backbone == 'xception':
-        return xception.AlignedXception(output_stride, BatchNorm)
-    elif backbone == 'drn':
-        return drn.drn_d_54(BatchNorm)
-    elif backbone == 'mobilenet':
-        return mobilenet.MobileNetV2(output_stride, BatchNorm)
-    else:
-        raise NotImplementedError
